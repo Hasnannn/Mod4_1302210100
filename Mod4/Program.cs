a4636745742d07855c9f016001fcf3a7e93eacf4
Hasnannn <hasnanh.7113@gmail.com>
2023-03-13 08:25:23 +0700

    Update Program.cs

--- Mod4/Program.cs
+++ Mod4/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class KodeBuah
+{
+    private readonly Dictionary<string, string> NamaBuah = new Dictionary<string, string> {
+        {"Apel", "A00"}, {"Aprikot","B00"}, {"Alpukat", "C00"}, {"Pisang", "D00"},
+        {"Paprika","E00"}, {"Blackberry", "F00"}, {"Ceri", "H00"}, {"Kelapa", "I00"},
+        {"Jagung", "J00"}};
+
+    public string GetKodeBuah(string Id)
+    {
+        if (KodeBuah.TryGetValue(Id, out string Kode))
+        {
+            return string.Empty;
+        }
+        else
+        {
+            return Kode;
+        }
+    }
+}
+class MainProgram
+{
+    static void Main(string[] args)
+    {
+        KodeBuah KodeBuah = new KodeBuah();
+
+        string Buahan = KodeBuah.GetKodeBuah("Apel");
+        Console.WriteLine(Buahan);
+
+    }
+}
